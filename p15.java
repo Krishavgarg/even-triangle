c6658badc8535f916f3628f33b4917211e8cca4e
AVIRAL108 <vrlgarg@gmail.com>
2021-11-27 14:47:44 +0530

    eigth commit

---
+++ p15.java
@@ -0,0 +1,12 @@
+public class p15
+{public static void main()
+    {int s=2;
+        int k=s;
+        {for(int i=1;i<=4;i++)
+            {for(int j=1;j<=i;j++)
+                {
+                System.out.print(k);
+            k=k+2;}
+                {System.out.println();
+                s=s+i;}
+                }}}}
